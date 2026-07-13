ae5c3231f08f420c88484f928820ad4ed3f0ee0f
SorshaPredan <sorsha.predan@gmail.com>
2026-07-13 10:39:53 +0200

    Update Script.R

--- Script.R
+++ Script.R
@@ -53,18 +53,45 @@ plot.crn(BeechChron)
 head(BeechTRW)
 class(BeechTRW)
 BeechTRW <- as.data.frame(BeechTRW)
+rownames(BeechTRW) <- 1812:2018
 BeechTRWdetrend <- detrend(BeechTRW, method = "Spline", nyrs = 30)
 BeechChron<-chron(BeechTRWdetrend,prefix = "AVG", biweight = TRUE, prewhiten = FALSE)
 plot.crn(BeechChron)
 range(time(BeechChron))
-range(as.numeric(rownames(BeechChron)))
 
-plot(dcc(BeechChron, prec, selection = -6:9, method = "correlation",
-           timespan = c(1930,1990), var_names = "precipitation", boot = "std"))
+plot(dcc(BeechChron, prec, selection = -6:9,method = "correlation",
+           timespan = c(1930, 1990), var_names = "precipitation", boot = "std"))
 
 plot(dcc(BeechChron, temp, selection = -6:9, method = "correlation",
-         timespan = c(1930,2018), var_names = "temperature", boot = "std"))
+           timespan = c(1930,2018), var_names = "temperature", boot = "std"))
+
+
+### analisi clima-crescita
+period <- c(1930, 1990)
+month <- -6:9
+plot(dcc(BeechChron,prec,selection = month, method = "correlation", 
+            timespan = period, var_names = "precipitation", boot = "std"))
+
+1) Periodo lungo (clima recente completo): 1930 - 1990
+plot(dcc(BeechChron, prec, selection = -6:9, method = "correlation",
+           timespan = c(1930,1990), var_names = "precipitation", boot = "std"))
+plot(dcc(BeechChron, temp, selection = -6:9, method = "correlation",
+         timespan = c(1930,1990), var_names = "temperature", boot = "std"))
+
+2) Periodo storico più ampio: 1891 - 1990
+plot(dcc(BeechChron, prec, selection = -6:9, method = "correlation",
+           timespan = c(1891,1990), var_names = "precipitation", boot = "std"))
+plot(dcc(BeechChron, temp, selection = -6:9, method = "correlation",
+         timespan = c(1891,1990), var_names = "temperature", boot = "std"))
+
+3) Periodo recente (cambiamento climatico): 1950 - 1990
+plot(dcc(BeechChron, prec, selection = -6:9, method = "correlation",
+           timespan = c(1950,1990), var_names = "precipitation", boot = "std"))
+plot(dcc(BeechChron, temp, selection = -6:9, method = "correlation",
+         timespan = c(1950,1990), var_names = "temperature", boot = "std"))
+
 
 combiclim <-list(temp , prec)
+ClimaBegin <- 1891
+ClimaEnd <- 1990
 plot(seascorr(BeechChron,combiclim,timespan=c(ClimaBegin,ClimaEnd),complete = 8, season_lengths = c(3),primary = 1,secondary = 2, ci = 0.05))
-
